e19e515eb3b427c03edb9d449519352753c420eb
Sherry.Li <>
2020-11-27 11:45:11 +0800

    again

---
+++ src/index.js
@@ -0,0 +1,1 @@
+const spyOr = require("spy");
---
+++ src/spy/index.js
@@ -0,0 +1,28 @@
+/**
+ * 監聽滾動在範圍的內容
+ */
+module.exports = function spyOr(list, setActive) {
+  /**
+   * 監聽位置參數
+   */
+  const options = {
+    rootMargin: "-45% 0px -45%",
+    threshold: [0, 0.5, 1],
+  };
+
+  /**
+   * 顯示範圍改變時，改變Active Item
+   */
+  const observerCallbacks = (entries) => {
+    if (!entries[0].isIntersecting) return;
+    setActive(entries[0].target);
+  };
+
+  const observer = new IntersectionObserver(observerCallbacks, options);
+
+  if (list) {
+    list.map((element) => {
+      observer.observe(element);
+    });
+  }
+};
